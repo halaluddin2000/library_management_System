6d2e8c70ce632eb55eb8aa33d064fe5c8601d3e5
halaluddin2000 <helaluddin7001@gmail.com>
2025-07-08 22:21:46 +0600

    borrow router added

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application, Request, Response } from "express";
 import { BooksRoutes } from "./controllers/books.controllers";
+import { BorrowsRouter } from "./controllers/borrow.controller";
 
 const app: Application = express();
 
 app.use(express.json());
 
 app.use("/books", BooksRoutes);
+app.use("/borrow", BorrowsRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Note App");
---
+++ src/controllers/borrow.controller.ts
@@ -0,0 +1,82 @@
+import express, { Request, Response } from "express";
+import { Book } from "../models/books.model";
+import { Borrow } from "../models/borrow.model";
+
+export const BorrowsRouter = express.Router();
+
+BorrowsRouter.post("/", async (req: Request, res: Response) => {
+  try {
+    const { book: bookId, quantity, dueDate } = req.body;
+
+    const book = await Book.findById(bookId);
+    if (!book) throw new Error("Book not found");
+    if (book.copies < quantity) throw new Error("Not enough copies available");
+
+    //Update Book
+    book.copies -= quantity;
+    if (book.copies === 0) {
+      book.available = false;
+    }
+    await book.save();
+
+    const borrow = await Borrow.create({
+      book: bookId,
+      quantity,
+      dueDate,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Book borrowed successfully",
+      data: borrow,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+      error,
+    });
+  }
+});
+BorrowsRouter.get("/", async (req: Request, res: Response) => {
+  try {
+    const summary = await Borrow.aggregate([
+      {
+        $group: {
+          _id: "$book",
+          totalQuantity: { $sum: "$quantity" },
+        },
+      },
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "_id",
+          as: "bookInfo",
+        },
+      },
+      { $unwind: "$bookInfo" },
+      {
+        $project: {
+          book: {
+            title: "$bookInfo.title",
+            isbn: "$bookInfo.isbn",
+          },
+          totalQuantity: 1,
+        },
+      },
+    ]);
+
+    res.status(201).json({
+      success: true,
+      message: "Book borrowed successfully",
+      data: summary,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+      error,
+    });
+  }
+});
