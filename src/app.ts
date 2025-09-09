609209ac3f6bc2e9fcf20d36caec9bc38ef8435a
halaluddin2000 <helaluddin7001@gmail.com>
2025-09-09 12:57:33 +0600

    app update

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
 import { BooksRoutes } from "./controllers/books.controllers";
 import { BorrowsRouter } from "./controllers/borrow.controller";
 
 const app: Application = express();
 
+app.use(cors());
+
+// Middleware for JSON parsing
 app.use(express.json());
 
+// Routes
 app.use("/books", BooksRoutes);
 app.use("/borrow", BorrowsRouter);
 
+// Test route
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Library App");
 });
